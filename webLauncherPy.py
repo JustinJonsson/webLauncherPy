d23906fb8b1eff3e2bd9e0d3cb25388c5433ad89
JustinJonsson <junk.jonsson@gmail.com>
2015-07-26 17:04:48 -0400

    add ini

--- webLauncherPy.py
+++ webLauncherPy.py
@@ -4,10 +4,10 @@ import configparser
 
 ctypes.windll.user32.MessageBoxW(0, "sup dude", "title", 1)
 config = configparser.ConfigParser()
-config.read()
+config.read(webLauncherPy.ini)
 
-browser1 = 
+browser1 = config.get("Browsers", "Browser1", fallback="iexplore.exe")
 
-browser2 = 
+browser2 = config.get("Browsers", "Browser2", fallback="chrome.exe")
 
 browser3 = 
