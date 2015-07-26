99636db3266a09041b03fa8ac09d5eaeffde8cfd
JustinJonsson <junk.jonsson@gmail.com>
2015-07-26 19:11:18 -0400

    Mostly done with v1
    Just need to figure out how to launch Win apps.

--- webLauncherPy.py
+++ webLauncherPy.py
@@ -1,13 +1,15 @@
 import sys
 import ctypes
 import configparser
+from subprocess import Popen
 
 def getdomain(url):
-    return 0
-
+    url = url.split("//")[1]
+    url = url.split("/")[0]
+    return str(url)
 
 def main(argv):
-    ctypes.windll.user32.MessageBoxW(0, "sup dude", "title", 1)
+
     config = configparser.ConfigParser()
     config.read("webLauncherPy.ini")
 
@@ -15,7 +17,15 @@ def main(argv):
     browser2 = config.get("Browsers", "Browser2", fallback="chrome.exe")
     domains1 = config.get("Domains", "Domains1", fallback="ukwdc-qcapp1:8080")
 
-    ctypes.windll.user32.MessageBoxW(0, "Browser1="+browser1,"title",1)
+    domain = getdomain(argv[0])
+    ctypes.windll.user32.MessageBoxW(0, domain,"title",0)
 
+    if domain in domains1:
+    	browserToRun = browser1
+    else:
+    	browserToRun = browser2
+
+    p = Popen(browserToRun, argv[0])
+    
 if __name__ == "__main__":
 	main(sys.argv[1:])
