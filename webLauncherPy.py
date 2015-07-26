51c250b709c6a71c543a96d59b6795df6ae90b95
JustinJonsson <junk.jonsson@gmail.com>
2015-07-26 17:43:10 -0400

    adding test links

--- webLauncherPy.py
+++ webLauncherPy.py
@@ -2,12 +2,20 @@ import sys
 import ctypes
 import configparser
 
-ctypes.windll.user32.MessageBoxW(0, "sup dude", "title", 1)
-config = configparser.ConfigParser()
-config.read(webLauncherPy.ini)
+def getdomain(url):
+    return 0
 
-browser1 = config.get("Browsers", "Browser1", fallback="iexplore.exe")
 
-browser2 = config.get("Browsers", "Browser2", fallback="chrome.exe")
+def main(argv):
+    ctypes.windll.user32.MessageBoxW(0, "sup dude", "title", 1)
+    config = configparser.ConfigParser()
+    config.read("webLauncherPy.ini")
 
-browser3 = 
+    browser1 = config.get("Browsers", "Browser1", fallback="iexplore.exe")
+    browser2 = config.get("Browsers", "Browser2", fallback="chrome.exe")
+    domains1 = config.get("Domains", "Domains1", fallback="ukwdc-qcapp1:8080")
+
+    ctypes.windll.user32.MessageBoxW(0, "Browser1="+browser1,"title",1)
+
+if __name__ == "__main__":
+	main(sys.argv[1:])
