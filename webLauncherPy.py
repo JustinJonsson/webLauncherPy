36324bbd8ef826904cc76f1921db3877f5b02a6e
JustinJonsson <junk.jonsson@gmail.com>
2015-07-26 16:21:12 -0400

    2nd

---
+++ webLauncherPy.py
@@ -0,0 +1,13 @@
+import sys
+import ctypes
+import configparser
+
+ctypes.windll.user32.MessageBoxW(0, "sup dude", "title", 1)
+config = configparser.ConfigParser()
+config.read()
+
+browser1 = 
+
+browser2 = 
+
+browser3 = 
